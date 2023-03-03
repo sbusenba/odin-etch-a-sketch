4380c0d195d78461640a41041a57cd6ef81fb698
Steven Busenbarrick <sbusenba@gmail.com>
2023-03-03 10:14:44 -0800

    create mode selector and switch statement for mode
    change mouseOver event to add switch statement
    to select effect

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 const container = document.querySelector('.container')
 const newButton = document.querySelector('.newButton')
 const input = document.querySelector('input')
-let gridSize = 16
 newButton.addEventListener('click',newButtonClick)
-input.addEventListener('change',newButtonClick)
+input.addEventListener('change',changeSize)
+let size = parseInt(input.value)
 function clearGrid(){
     while (container.firstElementChild)
     container.removeChild(container.firstElementChild)
-
 }
 
-function newButtonClick (){
+function newButtonClick (e){
+    e.preventDefault()
     clearGrid()
-    gridSize = Number(input.value)
-
-    
+    let gridSize = parseInt(input.value)
+    console.log(gridSize)
     drawGrid(gridSize)
-
 }
-function fadeToBlack(e){
-        this.classList.add('darkened')
+
+function changeSize(){
+    clearGrid()
+    let gridSize = parseInt(input.value)
+    console.log(gridSize)
+    drawGrid(gridSize)
+}
+
+
+function mouseOver(e){
+    let mode = document.querySelector('#mode-select').value
+    switch (mode) {
+        case 'black':
+            e.target.classList.add('darkened')
+            break;
+        case 'greyscale':
+
+            break;
+        case 'rainbow':
+
+            break;
+        case 'random':
+
+            break;
+    
+        default:
+            break;
+    }
+    
 }
 function drawGrid(width){
     for(let rowNum=0;rowNum<width;rowNum++){
@@ -28,7 +53,7 @@ function drawGrid(width){
         for(let columnNum=0;columnNum<width;columnNum++){
             let cell= document.createElement('div')
             cell.classList.add('cell')
-            cell.addEventListener('mouseenter',fadeToBlack)
+            cell.addEventListener('mouseenter',mouseOver)
             row.appendChild(cell)
 
         }
@@ -37,5 +62,5 @@ function drawGrid(width){
     }
 }
 
-drawGrid(gridSize)
+drawGrid(size)
 
